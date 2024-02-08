98c8982ba39096d2f09391825025b7cea883cbc6
shouryagautam <shourya@busy.mail.in>
2024-02-08 13:01:39 +0530

    Implemented Changes

--- main.go
+++ main.go
@@ -5,14 +5,19 @@ import (
 	"reflect"
 )
 
+//Merges the given interfaces
 func merge(arr interface{}, nn interface{}) (interface{},error) {
 
+	//Value is extacted from each interface using reflect
+	arrReflected := reflect.ValueOf(arr)
+	nnReflected := reflect.ValueOf(nn)
+	
+	//check for possible invalid inputs
 	if arr == nil {
 		if nn == nil {
 		return nil, fmt.Errorf("Nil Input")
 		} else {
-			nnReflected := reflect.ValueOf(nn)
-
+			//if 'arr' is nil return 'nn' either as it is or after putting in a slice
 			switch nnReflected.Kind() {
 			case reflect.Slice:
 				return nn,nil
@@ -22,39 +27,71 @@ func merge(arr interface{}, nn interface{}) (interface{},error) {
 		}
 	}
 
+	//Declare an array of interface to store the merged array
 	var out []interface{}
 
-	arrReflected := reflect.ValueOf(arr)
-	nnReflected := reflect.ValueOf(nn)
-
-	switch arrReflected.Kind() {
-	case reflect.Slice :
-		for i:= 0; i < arrReflected.Len(); i++ {
-			out = append(out,arrReflected.Index(i).Interface())
-		}
-	default:
-		out = append(out, arrReflected.Interface())
-    }
-
-	switch nnReflected.Kind() {
-	case reflect.Slice :
-		for i:= 0; i < nnReflected.Len(); i++ {
-			out = append(out,nnReflected.Index(i).Interface())
-		}
-	default:
-		out = append(out, nnReflected.Interface())
+	//If both 'arr' and 'nn' are slices then merge them efficiently
+	if arrReflected.Kind() == reflect.Slice && nnReflected.Kind() == reflect.Slice {
+		out = mergeEfficient(arrReflected,nnReflected,out)
+	} else {
+		//Else merge them individually
+		out = mergeSelect(arrReflected,out)
+		out = mergeSelect(nnReflected,out)
 	}
-
+	
+	//Return the merged array
 	return  out,nil
 }
 
+func mergeSelect(arr reflect.Value, out []interface{}) []interface{} {
+	//Determine the type of 'arr' and put it into out accordingly
+	switch arr.Kind() {
+	//Push each element of 'arr' if it a slice
+	case reflect.Slice :
+		for i:= 0; i < arr.Len(); i++ {
+			out = append(out,arr.Index(i).Interface())
+		}
+	default:
+	//Push the single element directly 
+		out = append(out, arr.Interface())
+    }
+
+	return out
+}
+
+func mergeEfficient(arrReflected reflect.Value, nnReflected reflect.Value, out []interface{}) []interface{} {
+	i := 0
+
+	//Since both are slices---push the values together
+	for ; i < arrReflected.Len() && i < nnReflected.Len(); i++ {
+		out = append(out,arrReflected.Index(i).Interface())
+		out = append(out,nnReflected.Index(i).Interface())
+	}
+
+	//Now push the remaining elements if any
+
+	for ; i < arrReflected.Len(); i++ {
+		out = append(out,arrReflected.Index(i).Interface())
+	}
+
+	for ; i < nnReflected.Len(); i++ {
+		out = append(out,nnReflected.Index(i).Interface())
+	}
+
+	return out
+
+}
+
 func main() {
-	//ar1 := []int{1, 2}
-	ar2 := []string{"a","abc"}
-	out , err := merge("th",ar2)
+	ar1 := []int{1, 2, 3}
+	ar2 := []string{"a","abc","efg","123"}
+
+	//function to merge two arrays of any type
+	out , err := merge(ar1,ar2)
+
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(out)
 
+	fmt.Println(out)
 }
